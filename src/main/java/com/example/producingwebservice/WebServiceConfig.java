feaa5bcf1e44fba1297201d5202e21104e3cf19f
wael2000 <weldoami@redhat.com>
2021-11-14 12:55:17 +0400

    changes

--- src/main/java/com/example/producingwebservice/WebServiceConfig.java
+++ src/main/java/com/example/producingwebservice/WebServiceConfig.java
@@ -23,18 +23,18 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		return new ServletRegistrationBean<>(servlet, "/ws/*");
 	}
 
-	@Bean(name = "countries")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+	@Bean(name = "workorders")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema workordersSchema) {
 		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-		wsdl11Definition.setPortTypeName("CountriesPort");
+		wsdl11Definition.setPortTypeName("WorkordersPort");
 		wsdl11Definition.setLocationUri("/ws");
 		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
-		wsdl11Definition.setSchema(countriesSchema);
+		wsdl11Definition.setSchema(workordersSchema);
 		return wsdl11Definition;
 	}
 
 	@Bean
-	public XsdSchema countriesSchema() {
+	public XsdSchema workordersSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("workorders.xsd"));
 	}
 }
